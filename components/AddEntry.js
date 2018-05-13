c2907e301bcdded339d3bca03c54499bcfcb9739
Johny Thomas <johnythomas007@gmail.com>
2018-05-13 22:35:29 +0530

    Added styles to AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,12 @@
 import { Ionicons } from "@expo/vector-icons"
 import React, { Component } from "react"
-import { Text, TouchableOpacity, View } from "react-native"
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Platform,
+  StyleSheet
+} from "react-native"
 import { connect } from "react-redux"
 import { addEntry } from "../actions"
 import { removeEntry, submitEntry } from "../utils/api"
@@ -13,10 +19,60 @@ import DateHeader from "./DateHeader"
 import TextButton from "./TextButton"
 import UdaciSlider from "./UdaciSlider"
 import UdaciSteppers from "./UdaciSteppers"
+import { white, purple } from "../utils/colors"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  }
+})
 
 const SubmitBtn = ({ onPress }) => (
-  <TouchableOpacity onPress={onPress}>
-    <Text>SUBMIT</Text>
+  <TouchableOpacity
+    style={
+      Platform.OS === "ios" ? styles.iosSubmitBtn : styles.androidSubmitBtn
+    }
+    onPress={onPress}
+  >
+    <Text style={styles.submitBtnText}>SUBMIT</Text>
   </TouchableOpacity>
 )
 
@@ -105,23 +161,28 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name="ios-happy-outline" size={100} />
+        <View style={styles.center}>
+          <Ionicons
+            name={Platform.OS === "ios" ? "ios-happy-outline" : "md-happy"}
+            size={100}
+          />
           <Text>You already logged your information for today</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton onPress={this.reset} style={{ padding: 10 }}>
+            Reset
+          </TextButton>
         </View>
       )
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === "slider" ? (
                 <UdaciSlider
