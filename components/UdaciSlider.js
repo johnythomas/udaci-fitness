739996a2190f921a8168b9dcc06ce9f9d80a0b70
Johny Thomas <johnythomas007@gmail.com>
2018-05-13 23:15:09 +0530

    Added styles to DateHeader, TextButton and UdaciSlider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,17 +1,34 @@
 import React from "react"
-import { Slider, Text, View } from "react-native"
+import { Slider, Text, View, StyleSheet } from "react-native"
+import { gray } from "../utils/colors"
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
 
 const UdaciSlider = ({ max, unit, step, value, onChange }) => (
-  <View>
+  <View style={styles.row}>
     <Slider
+      style={{ flex: 1 }}
       step={step}
       value={value}
       maximumValue={max}
       minimumValue={0}
       onValueChange={onChange}
     />
-    <Text>{value}</Text>
-    <Text>{unit}</Text>
+    <View style={styles.metricCounter}>
+      <Text style={{ fontSize: 24, textAlign: "center" }}>{value}</Text>
+      <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
+    </View>
   </View>
 )
 
